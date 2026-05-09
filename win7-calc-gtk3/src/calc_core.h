8b00e3f12b84d03755ed072526a1ae352b45d877
danr23 <117363821+danr23@users.noreply.github.com>
2026-05-09 12:32:47 -0700

    Update calc_core.h

--- win7-calc-gtk3/src/calc_core.h
+++ win7-calc-gtk3/src/calc_core.h
@@ -7,9 +7,9 @@ typedef struct {
     long double accumulator;
     long double current;
     Op pending_op;
-    int entering;
+    int entering; /* 1 if typing a number */
 } CalcState;
-/* List of functions that the calculator uses */
+
 void calc_init(CalcState *s);
 void calc_enter_digit(CalcState *s, int digit);
 void calc_enter_dot(CalcState *s);
@@ -18,11 +18,9 @@ void calc_press_equals(CalcState *s);
 void calc_clear_entry(CalcState *s);
 void calc_clear_all(CalcState *s);
 const char* calc_get_display(CalcState *s);
-/* useless feature from windows 7 */
 void calc_negate(CalcState *s);
-/* usefull features from windows 7 */
 void calc_sqrt(CalcState *s);
 void calc_percent(CalcState *s);
 void calc_reciprocal(CalcState *s);
-
+void calc_backspace(CalcState *s);
 #endif
